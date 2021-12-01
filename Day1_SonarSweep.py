139a4ea8c54037250d716db503a71b26cce47fbb
dredly <dredly@gmail.com>
2021-12-01 09:47:11 +0000

    Completed part 2 of Dec 1st exercise

--- Day1_SonarSweep.py
+++ Day1_SonarSweep.py
@@ -2,7 +2,14 @@ def sonar_sweep(depths):
     return len([d for idx, d in enumerate(depths) if idx > 0 and d > depths[idx - 1]])
 
 
+def group_by_n(depths, n):
+    sums = [
+        sum(depths[idx + 1 - n : idx + 1]) for idx in range(len(depths)) if idx >= 2
+    ]
+    return sums
+
+
 with open("depths_input.txt") as fin:
     depths = [int(line.strip()) for line in fin.readlines()]
 
-print(sonar_sweep(depths))
+print(sonar_sweep(group_by_n(depths, 3)))
