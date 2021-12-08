6f3030e235aef3aca519229d93996cd31a0eb487
dredly <dredly@gmail.com>
2021-12-08 07:57:58 +0000

    Completed part 1 of exercise 8

--- Day6_lanternfish_part2.py
+++ Day6_lanternfish_part2.py
@@ -1,18 +1,29 @@
 from itertools import chain
+from collections import Counter
 
 
 def simulate_lanternfish(current, days_remaining):
-    print(f'Days remaining: {days_remaining}')
+    print(f"Days remaining: {days_remaining}")
     if days_remaining == 0:
-        return len(list(current))
+        return current
     else:
         current = chain(*([6, 8] if f == 0 else [f - 1] for f in current))
         return simulate_lanternfish(current, days_remaining - 1)
+
+
+def get_single_fish_dict(days, fish_nums):
+    return {num: len(list(simulate_lanternfish([num], days))) for num in fish_nums}
 
 
 with open("lanternfish.txt") as fin:
     init_conditions = [int(x) for x in fin.read().split(",")]
 
 init_conditions_test = [3, 4, 3, 1, 2]
+fish_nums = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
-print(simulate_lanternfish([0], 256))
+fish_dict = get_single_fish_dict(128, fish_nums)
+print("Simulating")
+halfway = Counter(simulate_lanternfish(init_conditions, 128))
+total = sum((v * fish_dict[k] for (k, v) in halfway.items()))
+print("Done")
+print(total)
---
+++ Day8_Segment_Search.py
@@ -0,0 +1,18 @@
+def unique_digit_count(outputs):
+    unique_lengths = [2, 3, 4, 7]
+    return sum(
+        [
+            len([len(ele) for ele in outlist if len(ele) in unique_lengths])
+            for outlist in outputs
+        ]
+    )
+
+
+with open("segments.txt") as fin:
+    output_data = [
+        line.strip().split("|")[1][1:].split(" ") for line in fin.readlines()
+    ]
+
+print(unique_digit_count(output_data))
+
+# num_segments_dict = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
