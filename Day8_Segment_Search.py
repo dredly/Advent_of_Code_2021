3aac154779cc24220cc1f7c8388f04b6df02e0cb
Daniel Redly <redlydp@cardiff.ac.uk>
2021-12-08 18:00:23 +0000

    Worked on part 2 of Day 8 exercise

--- Day8_Segment_Search.py
+++ Day8_Segment_Search.py
@@ -8,6 +8,52 @@ def unique_digit_count(outputs):
     )
 
 
+def decode_inputs(inputs):
+    positions_dict = {}
+    decode_dict = {}
+    input_sets = [set(inp) for inp in inputs]
+    for s in input_sets:
+        if len(s) == 7:
+            decode_dict[8] = s
+        elif len(s) == 4:
+            decode_dict[4] = s
+        elif len(s) == 3:
+            decode_dict[7] = s
+        elif len(s) == 2:
+            decode_dict[1] = s
+
+    positions_dict['top'] = decode_dict[7] - decode_dict[1]
+    positions_dict['topleft'] = decode_dict[4] - decode_dict[1]
+    positions_dict['middle'] = decode_dict[4] - decode_dict[1]
+    positions_dict['topright'] = decode_dict[7].intersection(decode_dict[1])
+    positions_dict['bottomright'] = decode_dict[7].intersection(decode_dict[1])
+    for s in input_sets:
+        if len(s) == 5 and positions_dict['topright'].issubset(s):
+            decode_dict[3] = s
+        if len(s) == 6 and not(positions_dict['topright'].issubset(s)):
+            decode_dict[6] = s
+
+    positions_dict['topleft'] = decode_dict[4] - decode_dict[3]
+    positions_dict['middle'] = positions_dict['middle'] - \
+        positions_dict["topleft"]
+    positions_dict['bottomright'] = decode_dict[6].intersection(decode_dict[1])
+    positions_dict['topright'] = positions_dict['topright'] - \
+        positions_dict['bottomright']
+    for s in input_sets:
+        if len(s) == 6 and s != decode_dict[6]:
+            if positions_dict['middle'].issubset(s):
+                decode_dict[9] = s
+            else:
+                decode_dict[0] = s
+        if len(s) == 5:
+            if positions_dict['topright'] not in s:
+                decode_dict[5] = s
+            if positions_dict['bottomright'] not in s:
+                decode_dict[2] = s
+    print(positions_dict)
+    print(decode_dict, sorted(decode_dict.keys()))
+
+
 with open("segments.txt") as fin:
     output_data = [
         line.strip().split("|")[1][1:].split(" ") for line in fin.readlines()
@@ -36,6 +82,9 @@ single_input_test = [
 
 single_output_test = ["cdfeb", "fcadb", "cdfeb", "cdbaf"]
 
-num_segments_dict = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
+num_segments_dict = {0: 6, 1: 2, 2: 5, 3: 5,
+                     4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
+
+print(decode_inputs(single_input_test))
 
 # print(unique_digit_count(output_data))
