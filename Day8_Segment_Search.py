77314f8d3abe84eb0f7f46eeca1efd28f1a79ab9
dredly <dredly@gmail.com>
2021-12-08 09:01:07 +0000

    Started part 2 of Day 8 exercise

--- Day8_Segment_Search.py
+++ Day8_Segment_Search.py
@@ -13,6 +13,29 @@ with open("segments.txt") as fin:
         line.strip().split("|")[1][1:].split(" ") for line in fin.readlines()
     ]
 
-print(unique_digit_count(output_data))
+with open("segments.txt") as fin:
+    dacta_dict = {
+        tuple(line.strip().split("|")[1][1:].split(" ")): line.strip()
+        .split("|")[1][1:]
+        .split(" ")
+        for line in fin.readlines()
+    }
 
-# num_segments_dict = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
+single_input_test = [
+    "acedgfb",
+    "cdfbe",
+    "gcdfa",
+    "fbcad",
+    "dab",
+    "cefabd",
+    "cdfgeb",
+    "eafb",
+    "cagedb",
+    "ab",
+]
+
+single_output_test = ["cdfeb", "fcadb", "cdfeb", "cdbaf"]
+
+num_segments_dict = {0: 6, 1: 2, 2: 5, 3: 5, 4: 4, 5: 5, 6: 6, 7: 3, 8: 7, 9: 6}
+
+# print(unique_digit_count(output_data))
